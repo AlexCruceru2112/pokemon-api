52f2fb078c070e5dfaf346def14b3f6eeb35815d
Alexandru Cruceru <93651659+AlexCruceru2112@users.noreply.github.com>
2022-03-26 16:14:06 +0200

    json fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Home from "./components/Home";
 import Details from "./components/Details";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
